0303209dee70595ecc3be07ea31e91917a84c6e6
juliawalczak <juliawalczak@users.noreply.github.com>
2019-08-26 20:52:15 +0200

    Add file with class SearchData

---
+++ page_object_pattern/utils/search_data.py
@@ -0,0 +1,8 @@
+class SearchData:
+
+    def __init__(self, city, check_in, check_out, adults, child):
+        self.city = city
+        self.check_in = check_in
+        self.check_out = check_out
+        self.adults = adults
+        self.child = child
