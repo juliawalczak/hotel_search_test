b50c5054f84f4f2f34b6d64cce3386d88adc63ae
juliawalczak <juliawalczak@users.noreply.github.com>
2019-08-26 21:15:10 +0200

    Update methods

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -21,26 +21,26 @@ class SearchHotelPage:
     @allure.step("Setting city name to '{1}'")
     def set_city(self, city):
         self.logger.info("Setting city {}.".format(city))
-        self.driver.find_element(By.XPATH, self.search_hotel_span_xpath).click()
-        self.driver.find_element(By.XPATH, self.search_hotel_input_xpath).send_keys(city)
-        self.driver.find_element(By.XPATH, self.location_match_xpath).click()
+        self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_span_xpath).click()
+        self.driver.find_element(By.XPATH, SearchHotelLocators.search_hotel_input_xpath).send_keys(city)
+        self.driver.find_element(By.XPATH, SearchHotelLocators.location_match_xpath.format(city)).click()
         allure.attach(self.driver.get_screenshot_as_png(), name="set_city", attachment_type=AttachmentType.PNG)
 
     @allure.step("Setting date range from '{1}' to '{2}'")
     def set_date_range(self, check_in, check_out):
         self.logger.info("Setting check in: {checkin} and check out: {checkout} dates".format(checkin=check_in, checkout=check_out))
-        self.driver.find_element(By.NAME, self.check_in_input_name).send_keys(check_in)
-        self.driver.find_element(By.NAME, self.check_out_input_name).send_keys(check_out)
+        self.driver.find_element(By.NAME, SearchHotelLocators.check_in_input_name).send_keys(check_in)
+        self.driver.find_element(By.NAME, SearchHotelLocators.check_out_input_name).send_keys(check_out)
         allure.attach(self.driver.get_screenshot_as_png(), name="set_date_range", attachment_type=AttachmentType.PNG)
 
     @allure.step("Setting travelers - adults '{1}' and kids '{2}'")
     def set_travellers(self, adults, child):
         self.logger.info("Setting travellers adults: {adults} and child: {kids}".format(adults=adults, kids=child))
-        self.driver.find_element(By.ID, self.travellers_input_id).click()
-        self.driver.find_element(By.ID, self.adults_input_id).clear()
-        self.driver.find_element(By.ID, self.adults_input_id).send_keys(adults)
-        self.driver.find_element(By.ID, self.child_input_id).clear()
-        self.driver.find_element(By.ID, self.child_input_id).send_keys(child)
+        self.driver.find_element(By.ID, SearchHotelLocators.travellers_input_id).click()
+        self.driver.find_element(By.ID, SearchHotelLocators.adults_input_id).clear()
+        self.driver.find_element(By.ID, SearchHotelLocators.adults_input_id).send_keys(adults)
+        self.driver.find_element(By.ID, SearchHotelLocators.child_input_id).clear()
+        self.driver.find_element(By.ID, SearchHotelLocators.child_input_id).send_keys(child)
         allure.attach(self.driver.get_screenshot_as_png(), name="set_travellers", attachment_type=AttachmentType.PNG)
 
     def perform_search(self):
