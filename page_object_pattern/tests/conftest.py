7885d4f923117fcbef980c15ae13a221b5618b9c
juliawalczak <juliawalczak@users.noreply.github.com>
2019-08-26 19:32:38 +0200

    Update setup method

--- page_object_pattern/tests/conftest.py
+++ page_object_pattern/tests/conftest.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
-def setup():
+def setup(request):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(10)
     driver.maximize_window()
+    request.cls.driver = driver
     yield
     driver.quit()
