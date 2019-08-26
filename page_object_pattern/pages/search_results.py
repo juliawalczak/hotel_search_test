f8b5d0223925bc755f1209d41e87ff0fc3f5d95e
juliawalczak <juliawalczak@users.noreply.github.com>
2019-08-26 18:15:10 +0200

    Add logging

--- page_object_pattern/pages/search_results.py
+++ page_object_pattern/pages/search_results.py
@@ -1,17 +1,27 @@
 from selenium.webdriver.common.by import By
 from page_object_pattern.locators.locators import SearchResultsLocators
+import logging
 
 class SearchResultsPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
         self.hotel_names_xpath = SearchResultsLocators.hotel_names_xpath
         self.hotel_prices_xpath = SearchResultsLocators.hotel_prices_xpath
 
     def get_hotel_names(self):
         hotels = self.driver.find_elements(By.XPATH, self.hotel_names_xpath)
-        return [hotel.get_attribute("textContent") for hotel in hotels]
+        names = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Availabe hotels are: ")
+        for name in names:
+            self.logger.info(name)
+        return names
 
     def get_hotel_prices(self):
         prices = self.driver.find_elements(By.XPATH, self.hotel_prices_xpath)
-        return [price.get_attribute("textContent") for price in prices]
+        hotel_prices = [price.get_attribute("textContent") for price in prices]
+        self.logger.info("Hotel prices are: ")
+        for price in hotel_prices:
+            self.logger.info(price)
+        return hotel_prices
