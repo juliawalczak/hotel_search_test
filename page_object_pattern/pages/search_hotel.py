e2c7cac5f3004a10055ff04e7cc20cce9339a6e4
juliawalczak <juliawalczak@users.noreply.github.com>
2019-08-26 18:06:27 +0200

    Move locators to separate file

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -1,17 +1,18 @@
 from selenium.webdriver.common.by import By
+from page_object_pattern.locators.locators import SearchHotelLocators
 
 class SearchHotelPage:
     def __init__(self, driver):
         self.driver = driver
-        self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
-        self.search_hotel_input_xpath = "//div[@id='select2-drop']//input"
-        self.location_match_xpath =  "//span[text()='Dubai']"
-        self.check_in_input_name = "checkin"
-        self.check_out_input_name  = "checkout"
-        self.travellers_input_id =  "travellersInput"
-        self.adults_input_id =  "adultInput"
-        self.child_input_id = "childInput"
-        self.search_button_xpath = "//button[text()=' Search']"
+        self.search_hotel_span_xpath = SearchHotelLocators.search_hotel_span_xpath
+        self.search_hotel_input_xpath = SearchHotelLocators.search_hotel_input_xpath
+        self.location_match_xpath = SearchHotelLocators.location_match_xpath
+        self.check_in_input_name = SearchHotelLocators.check_in_input_name
+        self.check_out_input_name = SearchHotelLocators.check_out_input_name
+        self.travellers_input_id =  SearchHotelLocators.travellers_input_id
+        self.adults_input_id =  SearchHotelLocators.adults_input_id
+        self.child_input_id = SearchHotelLocators.child_input_id
+        self.search_button_xpath = SearchHotelLocators.search_button_xpath
 
     def set_city(self, city):
         self.driver.find_element(By.XPATH, self.search_hotel_span_xpath).click()
